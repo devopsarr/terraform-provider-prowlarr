c4a540f75adcac5f1670502374732d5070678aca
Fuochi <ffuochi@hotmail.com>
2023-02-01 16:03:21 +0400

    fix: read data source from request

--- internal/provider/system_status_data_source.go
+++ internal/provider/system_status_data_source.go
@@ -215,7 +215,7 @@ func (d *SystemStatusDataSource) Read(ctx context.Context, req datasource.ReadRe
 
 	status := SystemStatus{}
 	// status.write(response)
-	resp.Diagnostics.Append(resp.State.Get(ctx, &status)...)
+	resp.Diagnostics.Append(req.Config.Get(ctx, &status)...)
 	status.ID = types.Int64Value(1)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &status)...)
 }
