e6fc958c2945e22def1abe70ce4642e2981b4112
Fuochi <ffuochi@hotmail.com>
2023-02-02 09:42:17 +0400

    feat: add applications data source

---
+++ internal/provider/applications_data_source_test.go
@@ -0,0 +1,34 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccApplicationsDataSource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create a delay profile to have a value to check
+			{
+				Config: testAccApplicationResourceConfig("datasourceTest", "http://localhost:9696"),
+			},
+			// Read testing
+			{
+				Config: testAccApplicationsDataSourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckTypeSetElemNestedAttrs("data.prowlarr_applications.test", "applications.*", map[string]string{"prowlarr_url": "http://localhost:9696"}),
+				),
+			},
+		},
+	})
+}
+
+const testAccApplicationsDataSourceConfig = `
+data "prowlarr_applications" "test" {
+}
+`
