b4274e2da26fb3e0abd1b071678aefb616c1b85a
Fuochi <ffuochi@hotmail.com>
2023-02-22 09:37:55 +0400

    feat: add notification mailgun resource

---
+++ examples/resources/prowlarr_notification_mailgun/resource.tf
@@ -0,0 +1,11 @@
+resource "prowlarr_notification_mailgun" "example" {
+  on_health_issue       = false
+  on_application_update = false
+
+  include_health_warnings = false
+  name                    = "Example"
+
+  api_key    = "APIkey"
+  from       = "from_mailgun@example.com"
+  recipients = ["user1@example.com", "user2@example.com"]
+}
---
+++ internal/provider/notification_mailgun_resource_test.go
@@ -0,0 +1,67 @@
+package provider
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccNotificationMailgunResource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Unauthorized Create
+			{
+				Config:      testAccNotificationMailgunResourceConfig("error", "test@mailgun.com") + testUnauthorizedProvider,
+				ExpectError: regexp.MustCompile("Client Error"),
+			},
+			// Create and Read testing
+			{
+				Config: testAccNotificationMailgunResourceConfig("resourceMailgunTest", "test@mailgun.com"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("prowlarr_notification_mailgun.test", "from", "test@mailgun.com"),
+					resource.TestCheckResourceAttrSet("prowlarr_notification_mailgun.test", "id"),
+				),
+			},
+			// Unauthorized Read
+			{
+				Config:      testAccNotificationMailgunResourceConfig("error", "test@mailgun.com") + testUnauthorizedProvider,
+				ExpectError: regexp.MustCompile("Client Error"),
+			},
+			// Update and Read testing
+			{
+				Config: testAccNotificationMailgunResourceConfig("resourceMailgunTest", "test123@mailgun.com"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("prowlarr_notification_mailgun.test", "from", "test123@mailgun.com"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "prowlarr_notification_mailgun.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccNotificationMailgunResourceConfig(name, from string) string {
+	return fmt.Sprintf(`
+	resource "prowlarr_notification_mailgun" "test" {
+		on_health_issue                    = false
+		on_application_update              = false
+	  
+		include_health_warnings = false
+		name                    = "%s"
+		
+		api_key = "APIkey"
+		from = "%s"
+		recipients = ["test@test.com", "test1@test.com"]
+	}`, name, from)
+}
