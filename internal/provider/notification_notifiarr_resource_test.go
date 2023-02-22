cd7cc06f258bf1c16dc43446f5db1fd0b6a5434b
Fuochi <ffuochi@hotmail.com>
2023-02-22 09:36:38 +0400

    feat: add notification notifiarr resource

---
+++ internal/provider/notification_notifiarr_resource_test.go
@@ -0,0 +1,65 @@
+package provider
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccNotificationNotifiarrResource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Unauthorized Create
+			{
+				Config:      testAccNotificationNotifiarrResourceConfig("error", "key1") + testUnauthorizedProvider,
+				ExpectError: regexp.MustCompile("Client Error"),
+			},
+			// Create and Read testing
+			{
+				Config: testAccNotificationNotifiarrResourceConfig("resourceNotifiarrTest", "key1"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("prowlarr_notification_notifiarr.test", "api_key", "key1"),
+					resource.TestCheckResourceAttrSet("prowlarr_notification_notifiarr.test", "id"),
+				),
+			},
+			// Unauthorized Read
+			{
+				Config:      testAccNotificationNotifiarrResourceConfig("error", "key1") + testUnauthorizedProvider,
+				ExpectError: regexp.MustCompile("Client Error"),
+			},
+			// Update and Read testing
+			{
+				Config: testAccNotificationNotifiarrResourceConfig("resourceNotifiarrTest", "key2"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("prowlarr_notification_notifiarr.test", "api_key", "key2"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "prowlarr_notification_notifiarr.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccNotificationNotifiarrResourceConfig(name, key string) string {
+	return fmt.Sprintf(`
+	resource "prowlarr_notification_notifiarr" "test" {
+		on_health_issue                    = false
+		on_application_update              = false
+	  
+		include_health_warnings = false
+		name                    = "%s"
+	  
+		api_key = "%s"
+	}`, name, key)
+}
